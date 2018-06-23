c4f7cfc9d90881567b1ac54a350cc6d2cd484b74
vyahello <vjagello93@gmail.com>
2018-06-23 17:34:23 +0300

    Add app id

---
+++ server/forecast/__init__.py
@@ -0,0 +1,2 @@
+APP_ID: str = '7742ccb67df87111cbec7785057277f0'
+URL: str = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
--- server/forecast/forecasts.py
+++ server/forecast/forecasts.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Any, Callable
 from server.city import City
+from server.forecast import URL, APP_ID
 from server.web_api.requests import Request, SafeGet
 
 
@@ -16,10 +17,8 @@ class WeatherForecast(Forecast):
     """Represent of a specific weather forecast."""
 
     def __init__(self, city: City) -> None:
-        url: str = 'http://api.openweathermap.org/data/2.5/weather?q={}&' \
-                   'units=metric&appid=271d1234d3f497eed5b1d80a07b3fcd1'
         self._city: City = city
-        self._req: Callable[[City], Request] = lambda ct: SafeGet(url=url.format(ct.name))
+        self._req: Callable[[City], Request] = lambda ct: SafeGet(url=URL.format(ct.name, APP_ID))
 
     def latest(self) -> Dict[str, Any]:
         data = self._req(self._city).response().json()
